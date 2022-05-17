ad17247f19d819bd88374314e2f35076c56a35f7
ian <ian.murphy92@gmail.com>
2022-05-17 12:01:27 -0400

    commit tree 72401139b1e2195d93c404174b5d848614a2fffa

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+let num1 = 31
+let num2 = 2
+function multiply(num1, num2) {
+    return num1*num2;
+} 
+multiply = num1*num2
 
-// Write your code here
+
+function random() {
+   return Math.floor(Math.random() * 10) +1;
+}
+random = Math.floor(Math.random() * 10) +1
+let num3 = 20
+let num4 = 5
+function mod() {
+    return num3/num4;
+}
+mod = num3/num4
+
+function max() {
+    return Math.max(1,2,3,5,6,8,10,14,17,20)
+}
+max = Math.max(20)
